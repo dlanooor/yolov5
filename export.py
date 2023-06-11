72cc45aeb68298444bf8ac02885a0e5a4415510a
Ronald Grant <63400878+dlanooor@users.noreply.github.com>
2023-06-11 19:32:14 +0700

    Update export.py
    Signed-off-by: Ronald Grant <63400878+dlanooor@users.noreply.github.com>

--- export.py
+++ export.py
@@ -116,17 +116,27 @@ def try_export(inner_func):
 @try_export
 def export_torchscript(model, im, file, optimize, prefix=colorstr('TorchScript:')):
     # YOLOv5 TorchScript model export
-    LOGGER.info(f'\n{prefix} starting export with torch {torch.__version__}...')
-    f = file.with_suffix('.torchscript')
+#     LOGGER.info(f'\n{prefix} starting export with torch {torch.__version__}...')
+#     f = file.with_suffix('.torchscript')
 
+#     ts = torch.jit.trace(model, im, strict=False)
+#     d = {'shape': im.shape, 'stride': int(max(model.stride)), 'names': model.names}
+#     extra_files = {'config.txt': json.dumps(d)}  # torch._C.ExtraFilesMap()
+#     if optimize:  # https://pytorch.org/tutorials/recipes/mobile_interpreter.html
+#         optimize_for_mobile(ts)._save_for_lite_interpreter(str(f), _extra_files=extra_files)
+#     else:
+#         ts.save(str(f), _extra_files=extra_files)
+#     return f, None
+    LOGGER.info(f'\n{prefix} starting export with torch {torch.version}...')
+    fl = file.with_suffix('.torchscript.ptl')
     ts = torch.jit.trace(model, im, strict=False)
-    d = {'shape': im.shape, 'stride': int(max(model.stride)), 'names': model.names}
-    extra_files = {'config.txt': json.dumps(d)}  # torch._C.ExtraFilesMap()
-    if optimize:  # https://pytorch.org/tutorials/recipes/mobile_interpreter.html
-        optimize_for_mobile(ts)._save_for_lite_interpreter(str(f), _extra_files=extra_files)
+    d = {"shape": im.shape, "stride": int(max(model.stride)), "names": model.names}
+    extra_files = {'config.txt': json.dumps(d)} # torch._C.ExtraFilesMap()
+    if optimize:
+        optimize_for_mobile(ts)._save_for_lite_interpreter(str(fl), _extra_files=extra_files)
     else:
-        ts.save(str(f), _extra_files=extra_files)
-    return f, None
+        ts.save(str(fl), _extra_files=extra_files)
+    return fl, None
 
 
 @try_export
